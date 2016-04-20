a7efd99494917476e3801fb963d1132795f27871
fengkaiwhu <kaifeng@whu.edu.cn>
2016-04-20 18:13:28 +0800

    fisrt commit

--- js/booking.js
+++ js/booking.js
@@ -22,6 +22,7 @@ $(function () {
 
                     }
                 }
+                $("#group").selectmenu("refresh");
             },
             error:function(){
                 alert(data.content);
